66406687ba0d713507c357a8dc62974578ac29b1
ray <youkinnhfd@outlook.com>
2020-11-11 14:06:27 +0800

    202011111406

---
+++ upper/CarControl/ICar.h
@@ -0,0 +1,18 @@
+//
+// Created by Ray on 2020/11/11.
+//
+
+#ifndef UPPER_ICAR_H
+#define UPPER_ICAR_H
+
+class ICar{
+public:
+    ICar();
+    virtual ~ICar(){}
+    void setVelocity(double Vx,double Vy,double Vtheta);
+
+private:
+
+};
+
+#endif //UPPER_ICAR_H
