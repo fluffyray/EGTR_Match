84c0b791c5d1186e309990a05f8a6b4a544b0068
ray <youkinnhfd@outlook.com>
2020-11-25 21:37:25 +0800

    add some notes

--- upper/Communicate/botdevice.h
+++ upper/Communicate/botdevice.h
@@ -7,9 +7,16 @@
 
 #include "serialportdevice.h"
 
+/*
+ * @brief 小车底盘串口控制类，继承自串口设备类，用于设置小车速度
+ */
 class BotDevice: public SerialPortDevice{
 public:
     BotDevice();
+    /*
+     * @brief 设置小车底盘目标速度
+     * @parm velocities 要设置的速度矢量{Vx,Vy,Vtheta}
+     */
     void setTargetVelocity(std::vector<float> velocities);
 private:
 
--- upper/Communicate/serialportdevice.h
+++ upper/Communicate/serialportdevice.h
@@ -6,12 +6,19 @@
 #define EGTR_MATCH_UPPER_SERIALPORTDEVICE_H
 #include <vector>
 
+/*
+ * @brief 所有使用串口的设备（抽象的）的基类
+ */
 class SerialPortDevice{
 public:
     SerialPortDevice()=default;
     virtual ~SerialPortDevice()=default;
 
 protected:
+    /*
+     * @brief 串口传输函数
+     * @parm data 用于传输的数据
+     */
     void SerialPortDataTransmit(std::vector<unsigned char> data);
 
 private:
