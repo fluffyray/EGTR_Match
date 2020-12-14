82bc7796efb5fc047ec8d47e8f8900365b3ce8fc
ray <youkinnhfd@outlook.com>
2020-12-14 18:47:12 +0800

    fix bugs

--- lower/Botcontrol/picalculator.c
+++ lower/Botcontrol/picalculator.c
@@ -26,7 +26,7 @@ void getResults_PICalculator(PICalculator piCalculator,const float * targetValue
     float * Cbias = (float *)malloc(impPIcalculator->valueSize * sizeof(float ));
     for (int i = 0; i < impPIcalculator->valueSize - 1; ++i) {
         Cbias[i] = targetValue[i] - sampleValue[i];
-        value[i] = impPIcalculator->parmP * (Cbias[i] - impPIcalculator->PbiasValue[i])
+        value[i] += impPIcalculator->parmP * (Cbias[i] - impPIcalculator->PbiasValue[i])
                    + impPIcalculator->parmI * Cbias[i]; //calculate value
     }
     memcpy(impPIcalculator->PbiasValue, Cbias, impPIcalculator->valueSize * sizeof(float ));// save bias
