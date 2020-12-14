a4d02f2d49a46e4159b756aca77fcae4a30bccb5
ray <youkinnhfd@outlook.com>
2020-12-14 18:48:33 +0800

    add serial device

---
+++ lower/Botcontrol/serialcontrolor.h
@@ -0,0 +1,11 @@
+//
+// Created by Ray on 2020/12/9.
+//
+
+#ifndef LOWER_SERIALCONTROLOR_H
+#define LOWER_SERIALCONTROLOR_H
+#include "botmotor.h"
+
+void serialInfoHander(char* data,int length);
+
+#endif //LOWER_SERIALCONTROLOR_H
