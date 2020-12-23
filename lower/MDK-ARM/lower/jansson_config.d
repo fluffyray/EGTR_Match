1fd361e74e401d418ac2150d69a031c3ec7908c4
ray <youkinnhfd@outlook.com>
2020-12-23 12:16:05 +0800

    remove build files

--- lower/MDK-ARM/lower/jansson_config.d
+++
@@ -1,2 +0,0 @@
-lower\jansson_config.o: RTE\Data_Exchange\jansson_config.c
-lower\jansson_config.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- lower/MDK-ARM/lower/picalculator.d
+++
@@ -1,5 +0,0 @@
-lower\picalculator.o: ..\Botcontrol\picalculator.c
-lower\picalculator.o: ..\Botcontrol\picalculator.h
-lower\picalculator.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-lower\picalculator.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-lower\picalculator.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
