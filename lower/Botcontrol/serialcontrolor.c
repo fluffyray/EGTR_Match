16f121671b4f7fcbd5222511233f7acf2b44484c
ray <youkinnhfd@outlook.com>
2021-03-06 21:19:59 +0800

    Update serialcontrolor.c
    删除了机械臂角度控制，加入了屏幕信息控制

--- lower/Botcontrol/serialcontrolor.c
+++ lower/Botcontrol/serialcontrolor.c
@@ -4,6 +4,8 @@
 
 #include "serialcontrolor.h"
 #include "motordriver.h"
+#include "axedriver.h"
+#include "GUI_Paint.h"
 #include <jansson.h>
 #include <math.h>
 
@@ -14,35 +16,59 @@ float AXE_LEN = 1.93;
 float THROTTLE;
 float DIRECTION;
 char* MOVEMODE; 
+char* INFODATA;
 
 void serialInfoHander(char* data,int length)
 {
 	json_error_t error;
 	json_t *root;
 	json_t *bot;
+	json_t *info;
 	MOVEMODE = NULL;
 	root = json_loads(data,0,&error);
 	if(!json_is_object(root)) return;
+	info = json_object_get(root,"bot");
 	bot = json_object_get(root,"bot");
-	if(!json_is_object(bot)) return;
-	
-	THROTTLE = json_integer_value(json_object_get(bot,"throttle"));
-	DIRECTION = json_integer_value(json_object_get(bot,"direction"));
-	MOVEMODE = (char*)json_string_value(json_object_get(bot,"movemode"));
-	
+	if(json_is_object(bot))
+	{
+		THROTTLE = json_integer_value(json_object_get(bot,"throttle"));
+		DIRECTION = json_integer_value(json_object_get(bot,"direction"));
+		MOVEMODE = (char*)json_string_value(json_object_get(bot,"movemode"));
+		json_delete(bot);
+		if (NULL == MOVEMODE) return;
+		if(strcmp(MOVEMODE,"translation") == 0){
+			translation(THROTTLE,DIRECTION);
+			free(MOVEMODE);
+			return;
+		}
+		if(strcmp(MOVEMODE,"rotate") == 0){
+			rotate(THROTTLE,DIRECTION);
+			free(MOVEMODE);
+			return;
+		}
+	}
+	if(json_is_object(info))
+	{
+		INFODATA = (char*)json_string_value(json_object_get(info,"data"));
+		Paint_Clear(BLACK);
+		Paint_DrawString_EN(16,16,INFODATA,&Font16,BLACK,WHITE);
+	}
 	json_delete(root);
-	json_delete(bot);
-	if (NULL == MOVEMODE) return;
-	if(strcmp(MOVEMODE,"translation") == 0){
-		translation(THROTTLE,DIRECTION);
-		free(MOVEMODE);
-		return;
-	}
-	if(strcmp(MOVEMODE,"rotate") == 0){
-		rotate(THROTTLE,DIRECTION);
-		free(MOVEMODE);
-		return;
-	}
+//	axe = json_object_get(root,"axe");
+//	if(!json_is_array(axe)) return;
+	
+
+	
+//	int angSize = json_array_size(axe);
+//	for(int i = 0;i < angSize && i < 6;++i)
+//	{	
+//		ANGSET[i] =  json_integer_value(json_array_get(axe,i));
+//	}
+	
+	
+	
+//	json_delete(axe);
+
 }
 
 void translation(float throttle,float direction)
